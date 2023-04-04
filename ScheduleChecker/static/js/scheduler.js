57ffe8d6fe94a52d2a05ec31ee8277c443116165
SamoanJohn <johnschwenke@gmail.com>
2023-04-03 17:45:21 -0800

    completed course objects loaing from json.

--- ScheduleChecker/static/js/scheduler.js
+++ ScheduleChecker/static/js/scheduler.js
@@ -1,113 +1,111 @@
-// time grid object
-class timeData {
-  constructor() {
-    this.startTime = null;
-    this.endTime = null;
-    this.days = null;
-    this.css = null; // import the timeCell css
-  }
-  // duplicate function used in courseblock ****
-  loadCSS() {
-    this.css = document.querySelector('.timeCell');
-  }
-}
-
-
-// course object and json information retrieval 
-class course {
-  constructor() {
-    this.title = null;
-    this.start = null; 
-    this.end = null;
-    this.duration = null; // may not be needed, end - start.
-    this.subject = null;
-    this.major = null;
-    this.section = null;
-    this.days = null;
-    this.css = null; // not sure if this is used like this yet.
-  }
-
-  loadFromJSON(url) {
-    return fetch(url)
-      .then(data => {
-        this.title = data.title;
-        this.start = data.start; 
-        this.end = data.end;
-        this.duration = (this.end - this.start); // check on this 
-        this.subject = data.subject;
-        this.major = data.major;
-        this.section = data.section;
-        this.days = data.days;
-        return this;
-      })
-      .catch(error => {
-        console.error('Error loading JSON', error);
-      });
-  }
-
-  loadCSS() {
-    this.css = document.querySelector('.course-block');
-  }
-
-}
-
-/* Loop to create all course blocks, then will need to 
-  initiate into the corresponding timeCell. 
-/*
-const courseBlock =  new course();
-courseBlock.loadFromJSON(JSONresponse??);
-courseBlock.loadCSS();
+/* TODO
+courseElement: 
+- drag and drop not working
+- double click - show all object data, pop up?
+- same STime for multiple courseElements:
+  - courseElement CSS width = numOfElements / grid-cell width
+- height: (end time - start time) * 1px?
+- add CSS .conflict
+Conflict Box:
+- move to bottom?
+Format JSON data:
+- Del Mthd -> remove whitespace?
+- time: 0100pm -> mil time to make calculations on height?
+Export button and options to take updated course json data and export in correct format in excel.
 */
 
-/* Course Block element for testing, todo import json info, add css to courseblock object */
-const courseBlock = document.querySelector('.course-block');
-
-courseBlock.addEventListener('dragstart', dragStart);
-
-function dragStart(e) {
-    e.dataTransfer.setData('text/plain', e.target.id);
-    setTimeout(() => {
-        e.target.classList.add('hide');
-    }, 0);
-}
 
 
-/* target the time cell and highlight with border */
-const tcells = document.querySelectorAll('.timeCell');
-const gcells = document.querySelectorAll('.gridCell');
+// test json location, will need to find json URL from Austin
+fetch('/static/json/courses.json')
+  .then(response => response.json())
+  .then(data => {
+    // Loop through course objects and create draggable elements
+    data.forEach(course => {
+      const courseElement = document.createElement('div');
+      courseElement.classList.add('course-block');
+      courseElement.setAttribute('CRN', course.CRN);
+      courseElement.setAttribute('Subj', course.Subj);
+      courseElement.setAttribute('Crs', course.Crs);
+      courseElement.setAttribute('Sec', course.Sec);
+      courseElement.setAttribute('Title', course.Title);
+      courseElement.setAttribute('Days', course.Days);
+      courseElement.setAttribute('STime', course.STime);
+      courseElement.setAttribute('ETime', course.ETime);
+      courseElement.setAttribute('Bldg', course.Bldg);
+      courseElement.setAttribute('Room', course.Room);
+      courseElement.setAttribute('Sdate', course.SDate);
+      courseElement.setAttribute('Edate', course.EDate);
+      courseElement.setAttribute('Instructor', course.Instructor);
+      courseElement.setAttribute('DelMthd', course.DelMthd); // removed whitepsace
+      
+      // add Subj and Crs data for block text
+      const courseBlockText = document.createElement('div');
+      courseBlockText.classList.add('course-text');
+      courseBlockText.textContent = courseElement.getAttribute('Subj') + " " + courseElement.getAttribute('Crs');
+      courseElement.appendChild(courseBlockText);
 
-tcells.forEach((cell) => {
-    cell.addEventListener('dragenter', dragEnter)
-    cell.addEventListener('dragover', dragOver);
-    cell.addEventListener('dragleave', dragLeave);
-    cell.addEventListener('drop', drop);
-});
+      // Add drag and drop listeners
+      courseElement.addEventListener('dragstart', e => {
+        e.dataTransfer.setData('text/plain', course.Subj);
+      });
 
-function dragEnter(e) {
-    e.preventDefault();
-    e.target.classList.add('drag-over');
-}
+      courseElement.addEventListener('dragover', e => {
+        e.preventDefault();
+        e.dataTransfer.dropEffect = 'move';
+        const target = e.target;
+        target.classList.add('highlight');
+      });
 
-function dragOver(e) {
-    e.preventDefault();
-    e.target.classList.add('drag-over');
-}
+      courseElement.addEventListener('dragleave', e => {
+        e.preventDefault();
+        const target = e.target;
+        target.classList.remove('highlight');
+      });
 
-function dragLeave(e) {
-    e.target.classList.remove('drag-over');
-}
+      courseElement.addEventListener('drop', e => {
+        e.preventDefault();
+        const target = e.target;
+        target.classList.remove('highlight');
+        const subject = e.dataTransfer.getData('text/plain');
+        const startTime = target.getAttribute('data-time');
+        const day = target.getAttribute('data-day');
+        // Update course object start time
+        courseElement.setAttribute('STime', startTime);
+      });
 
-function drop(e) {
-    e.target.classList.remove('drag-over');
+      // Add course element to time cell
+      const day = courseElement.getAttribute('Days');
+      const time = courseElement.getAttribute('STime');
+      const timeSlot = document.querySelector(`[data-day="${day}"][data-time="${time}"]`);
+      timeSlot.appendChild(courseElement);
+    });
+  })
+  .catch(error => {
+    console.error('Error loading courses:', error);
+  });
 
-    // get the draggable element
-    const id = e.dataTransfer.getData('text/plain');
-    const draggable = document.getElementById(id);
 
-    // add it to the drop target
-    e.target.appendChild(draggable);
 
-    // display the draggable element
-    draggable.classList.remove('hide');
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
