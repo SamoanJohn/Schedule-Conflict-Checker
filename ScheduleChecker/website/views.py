a2ddb0d2e5667b87b7ecc6e6025447ad83ba30b5
apedwards3 <austinpedwards01@gmail.com>
2023-04-04 21:08:44 -0800

    Cleaned up view

--- ScheduleChecker/website/views.py
+++ ScheduleChecker/website/views.py
@@ -14,119 +14,110 @@ import time
 import os
 import re
 
-
-# Create your views here.
-
-# def calendar(request):
-#     return render(request, 'scheduler.html')
-
 def calendar(request):
+    # queries database tables MajorRequirements and Subjects used to populate dropdown menues 
     degree_major_data = MajorRequirements.objects.values('degree', 'major').annotate(count=Count('index')).order_by('degree', 'major')
     subject_data = Subjects.objects.values('subject', 'subj').order_by('subject', 'subj')
-
     context = {
         'degree_major_data': degree_major_data,
         'subject_data': subject_data
     }
     return render(request, 'scheduler.html', context)
 
+
 def get_major_requirements(request):
-    # get the url query "term=____majors=______subjects=______"
-    term = request.GET.get('term')
-
-    # split the majors and subjects.  Listed as "BS - Computer Science" and "CSCE - Computer Science and Computer Engineering"
+    # get term, majors, and subject from client
+    selected_term = request.GET.get('term')
     selected_majors = request.GET.get('majors')    
-    majors_list = selected_majors.split(',')
-    
     selected_subjects = request.GET.get('subjects')
-    subjects_list = []
+
+    # selected_subjects can be empty
+    selected_subj = []
     if selected_subjects:
-        subjects_list = selected_subjects.split(',')
-    subj = []
-    for item in subjects_list:
-        subject, subject_name = item.split(' - ')
-        subj.append(subject)
+        selected_subjects = selected_subjects.split(',')
+        for subject in selected_subjects:
+            abbreviation, full_name = subject.split(' - ')  # e.g., "CSCE - Computer Science and Computer Engineering"
+            selected_subj.append(abbreviation)
 
 
-    # retrieve the requirements for each selected major
+    # retrieve  requirements for each selected major
     courses = set()  # use a set to store all courses
-    for major in majors_list:
-        degree, major_name = major.split(' - ')
-        major_courses = set()  # use a set to remove duplicates within a major
-        for req in MajorRequirements.objects.filter(degree=degree.strip(), major=major_name.strip()):
-            major_courses.add(req.course)
-        courses.update(major_courses)  # add courses to the set of all courses
+    selected_majors = selected_majors.split(',')
+    for degree_major in selected_majors:
+        degree, major = degree_major.split(' - ')   # e.g., "BS - Computer Science" 
+        major_requirements = set()   # used to delete duplicate courses
+        for requirement in MajorRequirements.objects.filter(degree=degree.strip(), major=major.strip()):
+            major_requirements.add(requirement.course)
+        courses.update(major_requirements)  # append major_requirments to list of courses
 
-    # Remove courses in selected subjects
-    courses = [c for c in courses if not any(c.startswith(s + " ") for s in subj)]
-
-    # Sort the courses alphabetically
+    # Remove courses included in the selected subjects
+    courses = [course for course in courses if not any(course.startswith(subj + " ") for subj in selected_subj)]
     courses = sorted(courses)
 
-    # Initialize a dictionary to hold the course numbers for each subject
+    # create an dictionary of courses by subject
     courses_by_subject = {}
-
-    # Loop through each course subject in the list of requirements
     for course in courses:
-        # Split the course subject into its prefix and number components
-        subject, number = course.split()
-        # Add the current course number to the list for its subject
-        courses_by_subject.setdefault(subject, []).append(number)
+        # e.g., "CSCE A101" -> "CSCE", "A101"
+        subj, number = course.split()
+        courses_by_subject.setdefault(subj, []).append(number)
 
+
+    # create appropriate urls for scraping
     urls = []
-    # Loop through each subject and generate the corresponding URL
-    for subject, numbers in courses_by_subject.items():
+    for subj, numbers in courses_by_subject.items():
         # Combine the list of course numbers into a comma-separated string
-        course_numbers = ",".join(number.replace(" ", "+") for number in numbers)
-        # Generate the URL for the current subject and number combination
-        url = "https://curric.uaa.alaska.edu/scheduleSearch.php?term={}&subj={}&crse={}".format(term, subject, course_numbers)
+        course_numbers = ",".join(numbers.replace(" ", "+") for numbers in numbers)
+        url = "https://curric.uaa.alaska.edu/scheduleSearch.php?term={}&subj={}&crse={}".format(selected_term, subj, course_numbers)
+        urls.append(url)
+    for subj in selected_subj:
+        url = "https://curric.uaa.alaska.edu/scheduleSearch.php?term={}&subj={}".format(selected_term, subj)
         urls.append(url)
 
-    for s in subj:
-        url = "https://curric.uaa.alaska.edu/scheduleSearch.php?term={}&subj={}".format(term, s)
-        urls.append(url)
-
-    # Scrape the data using multiple threads
-    thread_count = min(5, len(urls))
+    thread_count = min(os.cpu_count(), len(urls))
+    # seperates urls into batches determined by avaliable resourses
     url_batches = [urls[i::thread_count] for i in range(thread_count)]
-    # create a list of futures
-    futures = []
-    dataframes = []
-
+    scraping_futures = []
+    temp_dataframe_list = []
 
     # scrape data for each batch of URLs using ThreadPoolExecutor
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        for batch in url_batches:
-            future = executor.submit(scrape_urls, batch)
-            futures.append(future)
+        for url_batch in url_batches:
+            # initiate threads in batches determined by avaliable resources
+            scraping_future = executor.submit(scrape_urls, url_batch)
+            scraping_futures.append(scraping_future)
 
     # retrieve the results of the futures and store them in a list of DataFrames
-    for future in concurrent.futures.as_completed(futures):
+    for scraping_future in concurrent.futures.as_completed(scraping_futures):
         try:
-            data = future.result()
-            if not data.empty:
-                dataframes.append(data)
+            scraped_dataframe = scraping_future.result()
+            temp_dataframe_list.append(scraped_dataframe)
+                
         except Exception as e:
-            print(f"Exception occurred: {e}")
-            dataframes.append(pd.DataFrame())
+            print(f"Threading exception occurred: {e}")
+            # dataframes.append(pd.DataFrame())
 
-    # Concatenate dataframes into a single dataframe
-    class_data_df = pd.concat(dataframes)
-
-    class_data_df = class_data_df.dropna(how='all')
-
+    # Concatenate list of dataframes into a single dataframe
+    class_data_df = pd.concat(temp_dataframe_list)
+    class_data_df = class_data_df.dropna(how='all') # deletes any empty rows
     class_data_df = class_data_df.rename(columns={'Del Mthd': 'DelMthd'})
-    # some data cleanup functions
+
+    # some data cleanup functions. the university data isnt very consistent.  this is just to extract the important information
     clean_whitespace = lambda x: re.sub(r'\s{2,}', ' ', x.strip()) if isinstance(x, str) else x
-    clean_duplicates = lambda x: re.sub(r'(\S+)\1(?!\S)', r'\1', x) if isinstance(x, str) else x
-    clean_online = lambda s: s.replace("ONLINE", "") if s != "ONLINE" else s
+
+    # if it includes the string ONLINE + something else, delete ONLINE e.g., "209ONLINE" -> "209"
+    clean_online = lambda s: s.replace("ONLINE", "") if s != "ONLINE" else s    
+
+    # if it includes the string DIST + something else, delete DIST e.g., "ECB DIST" -> ECB
     clean_dist = lambda s: s.replace("DIST", "") if s != "DIST" else s
 
-    # Apply the cleanup function to dataframe
-    class_data_df = class_data_df.applymap(clean_whitespace)
+    # all this does is deletes any duplicate string. e.g., "MWMW" -> "MW", "10/2110/21" -> "10/21", or "0130pm0130pm -> 0130pm"
+    clean_duplicates = lambda x: re.sub(r'(\S+)\1(?!\S)', r'\1', x) if isinstance(x, str) else x
+
+    # Apply the cleanup functions to dataframe
     class_data_df[['Days', 'STime', 'ETime', 'SDate', 'EDate']] = class_data_df[['Days', 'STime', 'ETime', 'SDate', 'EDate']].applymap(clean_duplicates)
     class_data_df[['Bldg']] = class_data_df[['Bldg']].applymap(clean_dist)
     class_data_df[['Room']] = class_data_df[['Room']].applymap(clean_online)
+    class_data_df = class_data_df.applymap(clean_whitespace)
     class_data_df[['Subj', 'Crs', 'Sec']] = class_data_df['Subj Crs Sec'].str.split(' ', expand=True)
     class_data_df = class_data_df.loc[:, ['CRN', 'Subj',"Crs","Sec","Title","Days","STime","ETime","Bldg","SDate","EDate","Instructor","DelMthd"]]
 
@@ -141,60 +132,43 @@ def scrape_urls(urls):
     options.add_argument('--disable-gpu')
     driver = webdriver.Chrome(executable_path=driver_path, options=options)
 
-    dataframes = []
-
+    temp_dataframe_list = []
     try:
         for url in urls:
-            # Navigate to the URL
             url = str(url)
-            driver.get(url)
-
-            # Wait for JS to populate data
-            time.sleep(2)
-
-            # Parse the HTML
+            driver.get(url) # open url 
+            time.sleep(2)   # wait for js to populate
             html = driver.page_source
             soup = BeautifulSoup(html, 'html.parser')
-
-            # Extract the data
-            # extract the table
             table = soup.find('table', {'class': 'table table-striped table-condensed'})
 
+            # Extract headers
             headers = []
-            for th in table.find_all('th'):
+            for th in table.find_all('th'):     #<th></th>
                 headers.append(th.text.strip())
 
             # Extract the table rows and store them in a list of lists
             class_data = []
-            for tr in table.find_all('tr')[1:]:
-                # find row data
-                row = []
-                for td in tr.find_all('td'):
-                    row.append(td.text.strip())
+            for tr in table.find_all('tr')[1:]:     #<tr></tr>
+                row_data = []
+                for td in tr.find_all('td'):     #<td></td>
+                    row_data.append(td.text.strip())
+                class_data.append(row_data)
 
-                class_data.append(row)
+            scraped_dataframe = pd.DataFrame(class_data, columns=headers)
+            temp_dataframe_list.append(scraped_dataframe)
 
-            # Convert the list of lists to a pandas dataframe
-            df = pd.DataFrame(class_data, columns=headers)
-            dataframes.append(df)
-
-        # Quit the webdriver instance
         driver.quit()
 
         # Concatenate the dataframes
-        if dataframes:
-            df_combined = pd.concat(dataframes, ignore_index=True)
+        if temp_dataframe_list:
+            class_data_df = pd.concat(temp_dataframe_list)
         else:
-            df_combined = pd.DataFrame()
+            class_data_df = pd.DataFrame()
+        return class_data_df
 
-        return df_combined
-    
-    except UnexpectedAlertPresentException as e:
-        # Quit the webdriver instance
-        driver.quit()
-
-        # Log the error message
-        print(f"One or more URLs returned no class data")
-
-        # Return an empty dataframe
-        return pd.DataFrame()
+    # occurs when the query has no data in curric.  for example summer classes with limited listings
+    except UnexpectedAlertPresentException:
+        print(f"No data found for URL: {url}")
+        # Skip to the next URL
+        pass
